1ebad08155dfbd72af3827faf00e5e3de47eb057
Chamika Jayasinghe <jayasinghech.21@uom.lk>
2024-12-24 22:44:09 +0530

    started cart svc development - added Cart & CartItem models

---
+++ ms/services/cart-service/cart/src/main/java/chamika/cart/model/Cart.java
@@ -0,0 +1,69 @@
+package chamika.cart.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "t_cart")
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cart {
+
+    @Id
+    @SequenceGenerator(name = "cart_id", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "cart_id")
+    private long id;
+
+    @Column(nullable = false)
+    private long userId;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> items;
+
+    private BigDecimal totalAmount;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
+
+    // cart management methods
+    public void addItem(CartItem item) {
+
+        items.add(item);              // Add to list
+        item.setCart(this);           // * Set bidirectional relationship
+        calculateTotalAmountForCart(); // Update total
+
+    }
+
+    public void removeItem(CartItem item) {
+
+        items.remove(item);           // Remove from list
+        item.setCart(null);           // * Break relationship
+        calculateTotalAmountForCart(); // Update total
+    }
+
+    @PrePersist  // Calculate total amount before persisting
+    @PreUpdate  // Calculate total amount before updating
+    public void calculateTotalAmountForCart() {
+
+        totalAmount = items.stream()
+                .map(CartItem::getSubTotal)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+
+}
---
+++ ms/services/cart-service/cart/src/main/java/chamika/cart/model/CartItem.java
@@ -0,0 +1,66 @@
+package chamika.cart.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "t_cart")
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartItem {
+
+    @Id
+    @SequenceGenerator(name = "cart_item_id", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "cart_item_id")
+    private long id;
+
+
+    @Column(nullable = false)
+    private long productId;
+
+    @Column(nullable = false)
+    private String productName;
+
+    @Column(nullable = false)
+    @Min(1)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private BigDecimal unitPrice;
+
+    private BigDecimal subTotal;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Cart cart;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
+    @PrePersist
+    @PreUpdate
+    public void findSubTotal() {
+        if (quantity != null && unitPrice != null) {
+            subTotal = unitPrice.multiply(BigDecimal.valueOf(quantity));
+        }
+    }
+
+
+
+
+}
